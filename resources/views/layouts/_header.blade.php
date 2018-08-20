9b0055c74ab1a1f135c9f12209590b38503f32f2
imcavoy <yyijian601@gmail.com>
2018-08-20 11:18:40 +0800

    use bootstrap_v4 and init root page

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
+    <div class="container">
+        <a class="navbar-brand" href="{{url('/')}}">LaraBBS</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#app-navbar-collapse">
+            &#9776;
+        </button>
+
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav">
+                <!-- Authentication Links -->
+                <li class="nav-item">
+                    <a class="nav-link" href="#">登录</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">注册</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title','社区首页')
+
+@section('content')
+    <h1>首页内容设置</h1>
+@stop
